de9183c6895969bea5d02dfccbb044a6435dff0a
sleepytaco <abukhan363@gmail.com>
2021-06-08 11:57:09 -0500

    Added static files for bootstrap5 and jquery

--- UnTube/settings.py
+++ UnTube/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 from UnTube.secrets import SECRETS
 
@@ -153,6 +153,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
