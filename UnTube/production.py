62d772a379ff32eb60a45c704e1c3c3984c003cf
sleepytaco <abukhan363@gmail.com>
2021-12-03 21:31:29 +0000

    Site ID update

--- UnTube/production.py
+++ UnTube/production.py
@@ -5,7 +5,7 @@ SECRET_KEY = SECRETS['SECRET_KEY']
 
 # configure the domain name using the environment variable found on pythonanywhere
 ALLOWED_HOSTS = ['bakaabu.pythonanywhere.com', '127.0.0.1', 'untube.it'] if 'UNTUBE' in os.environ else ['bakaabu.pythonanywhere.com', 'untube.it']
-SITE_ID = 8
+SITE_ID = 9
 
 DEBUG = False
 CSRF_COOKIE_SECURE = True
