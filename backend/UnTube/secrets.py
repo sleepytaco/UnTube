44e2fe8da984a8b04857cb9e0479612b0ec0d719
Mohammed Khan <abukhan363@gmail.com>
2023-05-25 12:05:48 -0400

    update local settings template

--- backend/UnTube/secrets.py
+++
@@ -1,6 +0,0 @@
-# Make sure you change these before production or to run this project on your own machine ;)
-SECRETS = {"SECRET_KEY": 'django-insecure-ycs22y+20sq67y(6dm6ynqw=dlhg!)%vuqpd@$p6rf3!#1h$u=',
-           "YOUTUBE_V3_API_KEY": 'AIzaSyCBOucAIJ5PdLeqzTfkTQ_6twsjNaMecS8',
-           "GOOGLE_OAUTH_CLIENT_ID": "901333803283-1lscbdmukcjj3qp0t3relmla63h6l9k6.apps.googleusercontent.com",
-           "GOOGLE_OAUTH_CLIENT_SECRET": "ekdBniL-_mAnNPwCmugfIL2q",
-           "GOOGLE_OAUTH_SCOPES": ['https://www.googleapis.com/auth/youtube']}
--- backend/UnTube/settings/templates/settings.dev.py
+++ backend/UnTube/settings/templates/settings.dev.py
@@ -1,2 +1,7 @@
 DEBUG = True
 SECRET_KEY = "django-insecure-ycs22y+20sq67y(6dm6ynqw=dlhg!)%vuqpd@$p6rf3!#1h$u="
+
+# please fill these in with your own Google OAuth credentials for the app to run properly!
+YOUTUBE_V3_API_KEY = NotImplemented
+GOOGLE_OAUTH_CLIENT_ID = NotImplemented
+GOOGLE_OAUTH_CLIENT_SECRET = NotImplemented
