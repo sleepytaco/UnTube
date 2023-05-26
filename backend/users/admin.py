48a79c3246f31847ad848a60ff5a2689508950a7
Mohammed Khan <abukhan363@gmail.com>
2023-05-25 21:31:42 -0400

    add profile admin

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+from backend.users.models import Profile
+
 
 # Register your models here.
+class ProfileResource(resources.ModelResource):
+    class Meta:
+        model = Profile
+
+
+class ProfileAdmin(ImportExportModelAdmin):
+    resource_class = ProfileResource
+    list_display = ('untube_user', 'access_token', 'refresh_token')
+    list_filter = ('created_at',)
+
+
+admin.site.register(Profile, ProfileAdmin)
